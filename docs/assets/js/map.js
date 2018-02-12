5ae368649a4b2e9f32609135db1e162115fbe689
linkevery2s <legend1world@gmail.com>
2018-02-12 13:45:50 +0900

    up

--- docs/assets/js/map.js
+++ docs/assets/js/map.js
@@ -15,3 +15,16 @@ var test = new L.tileLayer('https://tile.geospatial.jp/nankaitrough/sindobunpu/{
 var map = L.map('map_canvas', {center: [34.791, 135.893], zoom: 5,layers: [tiri, test]});
 
 }
+
+function map_ini2(){
+
+   var map2 = new L.Map('re_map');                       
+		L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
+  		attribution: '&copy; <a href="http://maps.gsi.go.jp/development/ichiran.html" target="_blank">地理院タイル</a>'
+  		}).addTo(map2);
+
+   map2.setView([35.067, 137.189], 9);
+
+	new L.Control.Fullscreen().addTo(map2);
+
+}
