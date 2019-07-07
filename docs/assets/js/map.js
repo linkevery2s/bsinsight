01ac8e0ba3afcbcc6607d35061430f254c130230
仁志 <legend1world@gmail.com>
2019-07-07 17:25:02 +0900

    up

--- docs/assets/js/map.js
+++ docs/assets/js/map.js
@@ -3,8 +3,8 @@ var map;var basemaps; var p;var zoom;var hash;var url;var number;var marker; var
 
 function map_ini(){
 
-var tiri = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
+var tiri = L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://maps.gsi.go.jp/development/ichiran.html" target="_blank">地理院タイル</a>'
 	});
 
 var test = new L.tileLayer('https://tile.geospatial.jp/nankaitrough/sindobunpu/{z}/{x}/{y}.png', {
@@ -18,8 +18,8 @@ var map = L.map('map_canvas', {center: [34.791, 135.893], zoom: 5,layers: [tiri,
 
 function hazard_ini(){
 
-var tiri = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
+var tiri = L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://maps.gsi.go.jp/development/ichiran.html" target="_blank">地理院タイル</a>'
 	});
 
 var test = new L.tileLayer('https://disaportaldata.gsi.go.jp/raster/01_flood_l2_shinsuishin_kuni_data/{z}/{x}/{y}.png', {
