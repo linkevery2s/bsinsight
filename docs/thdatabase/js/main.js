b8e7c32a5c790662c94a1d90e7bc66efd697b914
仁志 <legend1world@gmail.com>
2019-08-24 20:39:16 +0900

    up

---
+++ docs/thdatabase/js/main.js
@@ -0,0 +1,9 @@
+var url = "https://linkevery2s.github.io/bsinsight/thdatabase/index.html";
+
+function line(){
+	location.href = "line://msg/text/"+ url;
+}
+
+function mail(){
+	location.href =  "mailto:?body=App URL " + url;
+}
