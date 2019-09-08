644c474455cd6f5996347c5ac36eec9f8dbf53d1
仁志 <legend1world@gmail.com>
2019-09-08 16:53:43 +0900

    up

---
+++ docs/tenki/js/tenki.js
@@ -0,0 +1,292 @@
+function hok(){
+  
+			var xhr = new XMLHttpRequest();
+			xhr.onload = function() {
+			
+			var xml = xhr.responseXML.documentElement;
+				
+			  //document.getElementById('data_result').value = xhr.responseXML.documentElement.nodeName;
+			  
+			  //document.getElementById('data_result').value = xhr.responseXML.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+
+			  alert(xml.getElementsByTagName("title")[0]);
+
+			  
+			}
+			xhr.open("GET", "https://www.data.jma.go.jp/developer/xml/feed/regular_l.xml");
+			xhr.responseType = "document";
+			xhr.send();
+
+}
+
+function ao(){
+	
+
+
+}
+
+function ak(){
+	
+
+
+}
+
+function iw(){
+	
+
+
+}
+
+function my(){
+	
+
+
+}
+
+function yg(){
+	
+
+
+}
+
+function fs(){
+	
+
+
+}
+
+function ib(){
+	
+
+
+}
+
+function to(){
+	
+
+
+}
+
+function gu(){
+	
+
+
+}
+
+function si(){
+	
+
+
+}
+
+function tb(){
+	
+
+
+}
+
+function tk(){
+	
+
+
+}
+
+function kg(){
+	
+
+}
+
+function ni(){
+	
+
+}
+
+function toyama(){
+	
+
+
+}
+
+function is(){
+	
+
+
+}
+
+function fukui(){
+	
+
+
+}
+
+function yam(){
+	
+
+
+}
+
+function ng(){
+	
+
+
+}
+
+function gif(){
+	
+
+
+}
+
+function sz(){
+	
+
+
+}
+
+function aic(){
+	
+
+
+}
+
+function mie(){
+	
+
+
+}
+
+function siga(){
+	
+
+
+}
+
+function kyot(){
+	
+
+
+}
+
+function os(){
+	
+
+
+}
+
+function hg(){
+	
+
+
+}
+
+function nr(){
+	
+
+
+}
+
+function waky(){
+	
+
+
+}
+
+function tt(){
+	
+
+}
+
+function smn(){
+	
+
+}
+
+function oky(){
+	
+
+
+}
+
+function hiro(){
+	
+
+
+}
+
+function yag(){
+	
+
+
+}
+
+function tok(){
+	
+
+
+}
+
+function kagawa(){
+	
+
+
+}
+
+function ehi(){
+	
+
+
+}
+
+function kochi(){
+	
+
+
+}
+
+function fukuoka(){
+	
+
+
+}
+
+function saga(){
+	
+
+
+}
+
+function nags(){
+	
+
+
+}
+
+function kuma(){
+	
+
+
+}
+
+function oita(){
+	
+
+
+}
+
+function miyz(){
+	
+
+
+}
+
+function kagos(){
+	
+
+
+}
+
+function okina(){
+	
+
+
+}
