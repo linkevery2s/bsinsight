ff5be3317c462b9f69ccf8c97aa81ae619131f52
仁志 <legend1world@gmail.com>
2020-02-04 12:59:42 +0900

    up

--- docs/assets/js/hinanlist.js
+++ docs/assets/js/hinanlist.js
@@ -2,6 +2,6 @@ function list(){
 
 	var num = document.getElementById("number").value;
 	
-	document.getElementById("hinanlist").innerHTML = '<iframe width="100%" height="700" scrolling="no" frameborder="no" src="hinanlist/' + num + '.html"></iframe>';
+	document.getElementById("hinanlist").innerHTML = '<iframe width="100%" height="700" scrolling="yes" frameborder="no" src="hinanlist/' + num + '.html"></iframe>';
 
 }
