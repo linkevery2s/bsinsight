212f3d866d4088204b54898758027b62b1f71ac8
仁志 <legend1world@gmail.com>
2020-10-24 14:42:56 +0900

    up

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -6,5 +6,4 @@ self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
 
-// Œ»ó‚Å‚ÍA‚±‚Ìˆ—‚ğ‘‚©‚È‚¢‚ÆService Worker‚ª—LŒø‚Æ”»’è‚³‚ê‚È‚¢‚æ‚¤‚Å‚·
 self.addEventListener('fetch', function(event) {});
