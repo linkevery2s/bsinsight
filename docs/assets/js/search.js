7c2f539b755f2b2b0bf3a9e23d507f99fcd6a6a2
仁志 <legend1world@gmail.com>
2021-03-02 17:57:58 +0900

    up

---
+++ docs/assets/js/search.js
@@ -0,0 +1,23 @@
+var kensaku; var i;
+
+function search2(){
+	document.getElementById("output").style.display = "block";
+	kensaku = document.getElementById("text").value;
+	$("#output").empty();
+	if(kensaku == ""){
+		}else{
+    			todou();
+    }
+}
+
+function todou(){
+	$(document).ready(function () {
+   		$.getJSON("https://linkevery2s.github.io/bsinsight/index2.json", function(data){
+        	for(var i in data){
+        		if(data[i].Jusho.indexOf(kensaku) != -1) {
+        		$("#output").append("<li class='lin'><a href='" + data[i].saigai + "'>" + data[i].Name + "</a><div class='setu'>" + data[i].Jusho + "</div></li>");
+        		}
+        	}
+    	});
+	});
+}
