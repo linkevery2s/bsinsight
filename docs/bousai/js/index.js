8113eff4730d942b41fa491e3d0ed75842d7a04b
仁志 <legend1world@gmail.com>
2021-04-09 22:49:15 +0900

    Update index.js

--- docs/bousai/js/index.js
+++ docs/bousai/js/index.js
@@ -7,30 +7,19 @@ function init() {
     // 移動適用
 $('#targetImgDiv1').draggable();
 
-}
- 
-window.onload = init();
-
-function notice(){
-
-    // リサイズ適用
+// リサイズ適用
     $("#targetImgDiv2").resizable();
  
     // 移動適用
     $('#targetImgDiv2').draggable();
 
-}
-
-function notice2(){
-
-    // リサイズ適用
     $("#targetImgDiv3").resizable();
  
     // 移動適用
     $('#targetImgDiv3').draggable();
 
 }
-
+ 
 
 function kaijo(){
 
