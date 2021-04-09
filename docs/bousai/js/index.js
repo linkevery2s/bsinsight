bfc7c4a3e65d497fc58a887b352049951c5cc42e
仁志 <legend1world@gmail.com>
2021-04-09 22:39:25 +0900

    Update index.js

--- docs/bousai/js/index.js
+++ docs/bousai/js/index.js
@@ -7,12 +7,6 @@ function init() {
     // 移動適用
 $('#targetImgDiv1').draggable();
 
-    
-    // リサイズ適用
-    $(".targetImgDiv3").resizable();
- 
-    // 移動適用
-    $('.targetImgDiv3').draggable();
 }
  
 window.onload = init();
@@ -24,5 +18,15 @@ function notice(){
  
     // 移動適用
     $('#targetImgDiv2').draggable();
+
+}
+
+function notice2(){
+
+    // リサイズ適用
+    $("#targetImgDiv3").resizable();
+ 
+    // 移動適用
+    $('#targetImgDiv3').draggable();
 
 }
