ab26d22d36644e889954545b41e6624ffd0fd608
仁志 <legend1world@gmail.com>
2021-07-08 20:23:50 +0900

    Update hazardmap.js

--- docs/bousai/js/hazardmap.js
+++ docs/bousai/js/hazardmap.js
@@ -3,12 +3,12 @@ var map;var basemaps; var p;var zoom;var hash;var url;var number;var marker; var
 
 function map_ini(){
 
-var tiri = L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
+var tiri = L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg', {
 		attribution: '&copy; <a href="http://maps.gsi.go.jp/development/ichiran.html" target="_blank">地理院タイル</a>'
 	});
 
 var test = new L.tileLayer('https://tile.geospatial.jp/nankaitrough/sindobunpu/{z}/{x}/{y}.png', {
-    opacity: 0.7
+    opacity: 1
     });
 
 //地図を生成（初期表示の中心座標，ズームレベル，レイヤなどをオプションで設定）
