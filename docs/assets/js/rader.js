b48da96f1bb29dca3e269981f7390aa3df85fc4c
ひとし <legend1world@gmail.com>
2024-01-27 18:19:07 +0900

    Update rader.js

--- docs/assets/js/rader.js
+++ docs/assets/js/rader.js
@@ -1,19 +1,19 @@
 /* Windy */
-function windy_ame(){
-	document.getElementById('rader_now1').innerHTML = '<iframe width="100%" height="500" src="https://embed.windy.com/embed2.html?lat=37.003&lon=136.978&zoom=5&level=surface&overlay=rain&menu=&message=true&marker=&calendar=&pressure=true&type=map&location=coordinates&detail=&detailLat=37.003&detailLon=136.978&metricWind=default&metricTemp=default&radarRange=-1" frameborder="0" id="yout"></iframe>';
+function windy_ame() {
+	document.getElementById('rader_now1').innerHTML = '<iframe width="100%" height="500" src="https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=5&overlay=wind&product=ecmwf&level=surface&lat=38.417&lon=137.294" frameborder="0" id="yout"></iframe>';
 
 }
 
 /* 気象庁 */
-function ks_ame(){
+function ks_ame() {
 	document.getElementById('rader_now').innerHTML = '<iframe width="100%" height="500" src="krader/index.html" frameborder="0" id="yout"></iframe>';
 
 }
 
-function rader(){
+function rader() {
 
-windy_ame();
+	windy_ame();
 
-ks_ame();
+	ks_ame();
 
 }
